3eac0b349d89184ae27493b0b103af4b4934c5a7
Isaac Rhett <izcet@users.noreply.github.com>
2025-02-24 14:03:12 -0500

    styling fixes after feedback
    - corrected the second page sidebar background
    - duplicated contact info to the second page
    - improved build script to assist targeted builds

--- build.sh
+++ build.sh
@@ -2,10 +2,18 @@
 
 set -euxo pipefail
 
+TIMESTAMP="$(date +%Y%m%d%H%M)"
+COMPANY="${1:-generic}"
+
+# build the resume
 pdflatex main.tex
 
+# clean up intermediate steps
 mkdir -p out
 mv *.out *.log *.aux out/
 
+# render the newly built resume
 ps -ax | grep -i okular | grep -v grep || okular main.pdf &
 
+# create a release file
+cp main.pdf "Isaac_Rhett_${TIMESTAMP}_${COMPANY}.pdf"
